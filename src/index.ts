9f29108180a9fc50a7a533f4d1c993238b6c155c
James Hsi <hsidori@gmail.com>
2025-06-07 19:45:03 -0700

    update example

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,10 @@
 import { _createDesirePath } from "./core";
-import { getIntentPolygons, isValidPolygon, getBoxFromElement } from "./geometry";
 import type { TrailwindConfig } from "./types/core";
 import type { Point, Polygon } from "./types/geometry";
 
 export function createDesirePath(config: TrailwindConfig) {
     return _createDesirePath(config);
 }
-
-// Export core geometry functions
-export { getIntentPolygons, isValidPolygon, getBoxFromElement };
 
 // Export types
 export type { TrailwindConfig, Point, Polygon };
